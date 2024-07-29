32d0e1de333bd4c78b0e603b62b7e835308d8add
annyounghyun <hiyh6974@gmail.com>
2024-07-29 18:00:45 +0900

    test commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class Main {
+
+    
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        int ar[] = new int [5];
+        for (int i = 0; i < ar.length; i++) {
+            int i1 = ar[i];
+            
+        }
+
+        for (int i : ar) {
+            
+        }
+
+        int[] ints = new int[69];
+        
+        int a = 5;
+        int b = 6;
+        int i = a + b;
+
+        System.out.println();
+    }
+}
